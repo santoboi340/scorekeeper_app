557315f1094873072fea9698edb68e3ce0d41a6e
chihabam <chihabam@gmail.com>
2025-03-25 22:54:57 -0500

    basic spring boot initialized

---
+++ src/main/java/com/scorepal/scorepal/ScorepalApplication.java
@@ -0,0 +1,13 @@
+package com.scorepal.scorepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorepalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scorepal/scorepal/ScorepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorepal.scorepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
